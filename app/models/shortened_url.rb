9eb3cd3bff18185be7a2eb057da8a00c58fff9f4
Ramsey Njire <ramseynjire@gmail.com>
2021-03-26 14:57:22 +0300

    write methods for shortening urls and storing them in the database

--- app/models/shortened_url.rb
+++ app/models/shortened_url.rb
@@ -1,4 +1,22 @@
+require 'securerandom'
 class ShortenedUrl < ApplicationRecord
     validates :long_url, presence: true
     validates :short_url, uniqueness: true
+
+    
+    
+    def shorten_url(submitter, long_url)
+        shortened_url = create!(long_url: long_url, short_url: random_code(long_url))
+        submitter.shortened_urls << shortened_url
+    end
+    
+    private
+    
+    
+    def random_code(long_url)
+        begin
+            short_url = SecureRandom.urlsafe_base64(long_url)
+        end while exists?(short_url: short_url)
+        short_url
+    end
 end
