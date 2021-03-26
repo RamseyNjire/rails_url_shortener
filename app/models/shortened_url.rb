07c8203a00cfa8e0c883fcc70f940e66bc84a5e4
Ramsey Njire <ramseynjire@gmail.com>
2021-03-26 15:38:05 +0300

    create a method for creating shortened_urls and attaching them to users

--- app/models/shortened_url.rb
+++ app/models/shortened_url.rb
@@ -9,15 +9,6 @@ class ShortenedUrl < ApplicationRecord
         foreign_key: :user_id,
         primary_key: :id
     )
-
-    
-    
-    def shorten_url(submitter, long_url)
-        shortened_url = create!(long_url: long_url, short_url: random_code(long_url))
-        submitter.shortened_urls << shortened_url
-    end
-    
-    private
     
     
     def random_code(long_url)
